14be654d69d9a60147e2d18298fa24181ac49f52
joris paule <joris.chenevas@free.fr>
2020-06-14 18:04:53 +0200

    partie fonctionnelle

--- laravel/resources/views/vendor/blogetc/sitewide/show_all_categories.blade.php
+++ laravel/resources/views/vendor/blogetc/sitewide/show_all_categories.blade.php
@@ -1,5 +1,8 @@
 <h5>Post Categories</h5>
 <ul class="nav">
+  <li class="nav-item">
+  <a class='nav-link' href='{{ route('blogetc.index') }}'>Aucune catégorie</a>
+  </li>
     @foreach(\WebDevEtc\BlogEtc\Models\BlogEtcCategory::orderBy("category_name")->limit(200)->get() as $category)
         <li class="nav-item">
             <a class='nav-link' href='{{$category->url()}}'>{{$category->category_name}}</a>
