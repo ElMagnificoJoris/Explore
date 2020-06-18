97bf6ee96c7c094c4a550c53803ca52e594cb304
joris paule <joris.chenevas@free.fr>
2020-06-18 16:54:59 +0200

    arrangement css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,10 +81,10 @@
                         </li>
                         @guest
                             <li class="nav-item">
-                                <a class="navl" href="{{ route('login') }}">{{ __('Connexion') }}</a>
+                                <a class="navl connexnav" href="{{ route('login') }}">{{ __('Connexion') }}</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Inscription') }}</a>
+                                <a class="navl inscrinav" href="{{ route('register') }}">{{ __('Inscription') }}</a>
                             </li>
                         @else
                             <li class="nav-item dropdown">
